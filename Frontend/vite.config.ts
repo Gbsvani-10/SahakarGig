5de26380e2b372caaff6aec836213b2f9ca80a44
Jyothsna Sri Relangi <jyothsnasrirelangi999@gmail.com>
2026-09-09 09:07:01 +0530

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -18,6 +18,8 @@ export default defineConfig(() => {
       },
     
     server: {
+       allowedhosts:
+      ["sahakargig-jl19.onrender.com"],
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
